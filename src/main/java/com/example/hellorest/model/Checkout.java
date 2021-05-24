8ab4306713f091f5118add74fd10dbce60c037bc
Lukas Boril <borilluk@students.zhaw.ch>
2021-05-24 11:04:59 +0200

    status after tutorial, everything work

--- src/main/java/com/example/hellorest/model/Checkout.java
+++ src/main/java/com/example/hellorest/model/Checkout.java
@@ -1,4 +1,25 @@
 package com.example.hellorest.model;
 
+import javax.persistence.*;
+
+/**
+ * Model class for checkout
+ */
+@Entity
 public class Checkout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
